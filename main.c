a58de038f2e82fab7506ce4dc5386769b96dc57a
Jenifer Nunes <160186691+nunezz0@users.noreply.github.com>
2025-04-01 21:03:25 -0300

    main.c

--- main.c
+++ main.c
@@ -1,31 +1,31 @@
-/* Um lojista atribui o preço de venda dos itens de sualoja com um número
-racional (uma fração de inteiros).Este comerciante precisa de uma calculadora capaz derealizar as
-operações de soma, subtração,multiplicação e divisão de frações, a qual exibe oresultado como uma fração
- na forma mais simplificadapossível.
-Desenvolva um programa que implementeesta calculadora. */
+/* Um lojista atribui o preÃ§o de venda dos itens de sualoja com um nÃºmero
+racional (uma fraÃ§Ã£o de inteiros).Este comerciante precisa de uma calculadora capaz derealizar as
+operaÃ§Ãµes de soma, subtraÃ§Ã£o,multiplicaÃ§Ã£o e divisÃ£o de fraÃ§Ãµes, a qual exibe oresultado como uma fraÃ§Ã£o
+ na forma mais simplificadapossÃ­vel.
+Desenvolva um programa que implementeestaÂ calculadora. */
 
 #include <stdio.h>
-//biblioteca para realizar operações de entrada e saída.
+//biblioteca para realizar operaÃ§Ãµes de entrada e saÃ­da.
 
 int main(){
-    double numerador1, denominador1, numerador2, denominador2; //Utilizando o tipo "double" para que os cálculos funcionem com melhor precisão, pois é mais adequado para as operações matemáticas utilizadas no programa.
-    char operador; //Palavra reservada para armazenar os operadores (+, -, *, /) solicitados.
+    double numerador1, denominador1, numerador2, denominador2; //Utilizando o tipo "double" para que os cÃ¡lculos funcionem com melhor precisÃ£o, pois Ã© mais adequado para as operaÃ§Ãµes matemÃ¡ticas utilizadas no programa.
+    char operador; //Palavra reservada para armazenar os operadores (+, -, *, /) solicitados. 
     double resultado; //Armazenamento dos resultados calculados.
 
-    printf("Digite o numerador da primeira fracao: "); //Solicitando o primeiro númerador e o primeiro denomiador.
+    printf("Digite o numerador da primeira fracao: "); //Solicitando o primeiro numerador e o primeiro denomiador.
     scanf("%lf", &numerador1);// "lf" utilizado para ler o valor inserido e armazenar.
     printf("Digite o denominador da primeira fracao: ");
     scanf("%lf", &denominador1);
 
-    printf("Escolha a operacao (+, -, *, /): "); //Solicitando um operador para realizar o cálculo desejado.
-    scanf(" %c", &operador); //"%c" para que seja possível a leitura do caractere do operador.
+    printf("Escolha a operacao (+, -, *, /): "); //Solicitando um operador para realizar o cÃ¡lculo desejado.
+    scanf(" %c", &operador); //"%c" para que seja possÃ­vel a leitura do caractere do operador.
 
-    printf("Digite o numerador da segunda fracao: "); //Solicitando o segundo númerador e o segundo denomiador.
+    printf("Digite o numerador da segunda fracao: "); //Solicitando o segundo numerador e o segundo denomiador.
     scanf("%lf", &numerador2);
     printf("Digite o denominador da segunda fracao: ");
     scanf("%lf", &denominador2);
 
-    if (operador == '+') {  //"if" para controlar o fluxo de execução do programa
+    if (operador == '+') {  //"if" para controlar o fluxo de execuÃ§Ã£o do programa.
         resultado = (numerador1 / denominador1) + (numerador2 / denominador2);
         printf("Resultado da soma: %.2f\n", resultado);
     } else if (operador == '-') {
@@ -35,14 +35,14 @@ int main(){
         resultado = (numerador1 / denominador1) * (numerador2 / denominador2);
         printf("Resultado da multiplicacao: %.2f\n", resultado);
     } else if (operador == '/') {
-        if (numerador2 != 0 && denominador2 != 0) { //Função para dividir as frações convertendo-as para números racionais.
+        if (numerador2 != 0 && denominador2 != 0) { //FunÃ§Ã£o para dividir as fraÃ§Ãµes convertendo-as para nÃºmeros racionais.
             resultado = (numerador1 / denominador1) / (numerador2 / denominador2);
-            printf("Resultado da divisao: %.2f\n", resultado); //"%.2f\n" para formatar a saída dos números.
+            printf("Resultado da divisao: %.2f\n", resultado); //"%.2f\n" para formatar a saÃ­da dos nÃºmeros.
         } else {
-            printf("Erro: Divisao por zero nao é permitida.\n"); //Caso ocorra alguma divisão por zero.
+            printf("Erro: Divisao por zero nao Ã© permitida.\n"); //Caso ocorra alguma divisÃ£o por zero.
         }
     } else {
-        printf("Operador invalido!\n");//Mensagem caso o operador seja inválido.
+        printf("Operador invalido!\n");//Mensagem caso o operador seja invÃ¡lido.
     }
 
     return 0;
